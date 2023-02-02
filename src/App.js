e549c2d12bc9e321a182bc67dec0bccfb8bae75d
Yuval Kamani <yuvalkamani@gmail.com>
2023-02-02 18:08:34 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { motion } from "framer-motion";
+
+function App() {
+  return (
+    <div>
+      <motion.h1 className="heading">RFLECT</motion.h1>
+      <h2 className="startNow">Start journaling now</h2>
+    </div>
+  );
+}
+
+export default App;
